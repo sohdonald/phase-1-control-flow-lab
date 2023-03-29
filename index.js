5f643aeacb7f043a82f224ac7161153f3262bbc6
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-03-29 02:34:51 -0400

    else if statements work this time

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetNumber){
   // Write your code here!
+  if (feetNumber <= 400) {
+    return "This one is on me!";
+  }
+  
+  else if (feetNumber > 400, feetNumber <= 2000)
+  {
+    return "That will be twenty bucks.";
+  }
+  
+  else if (feetNumber > 2000, feetNumber <= 2500)
+  {
+    return "I will gladly take your thirty bucks.";
+  }
+
+  else {
+    return "No can do.";
+  }
+
 }
 
 function ternaryCheckCity(){
