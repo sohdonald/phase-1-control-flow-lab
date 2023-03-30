caa2e14cfbfd0b6158bb1266330e7182c264af56
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-03-30 17:17:46 -0400

    testing out case conditions

--- index.js
+++ index.js
@@ -42,15 +42,76 @@ since they are not same type.
 
 function switchOnCharmFromTip(tip){
   // Write your code here!
+  //tip = 'generous';
+
   switch (tip) {
-    case tip >= 5:
+    case 'generous':
     return "Thank you so much.";
     break;
 
-    case tip < 5:
+    case 'not as generous':
     return "Thank you.";
+    break;
 
-    case tip < 2:
-    return "Bye";
+    case 'thanks for everything':
+    return "Bye.";
   }
 }
+
+switchOnCharmFromTip('generous');
+// 'generous' is an argument, (tip) is the parameter
+// parameter is empty bottle, argument is what we put inside bottle
+
+function checkNumber(fishNumber) {
+
+  switch (fishNumber) {
+    case 6:
+      return "Tuna sushi";
+    break;
+
+    case 5:
+      return "Grilled eel";
+    break;
+
+    case 4:
+      return "Octopus";
+    break;
+
+    default:
+      return 'Sorry, not available today.';
+      
+  }
+}
+
+console.log(checkNumber(3));
+
+/*function lightSwitch(switchflip) {
+//switchflip is the input
+// inside {is the action}
+
+  switch(switchflip) {
+//switch is the action {cause it's inside curly braces}
+  case 'On':
+    return 'Light is on';
+    break;
+  
+  case 'Off':
+    return 'Light is off';
+    break;
+}
+}
+*/
+function lightSwitch(switchflip) {
+  //check for true or false
+  switch (switchflip) {
+    case true:
+      return 'Light is on';
+  //return is the output
+      break;
+    case false:
+      return 'Light is off'
+      break;
+  }
+}
+
+console.log(lightSwitch(true));
